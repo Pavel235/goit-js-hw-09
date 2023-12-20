4b7b5f49c7c55705dffa4a6e7ca0864870da7814
Pavel235 <crushsad@gmail.com>
2023-12-20 20:30:44 +0200

    upd js homework 09

---
+++ src/js/02-form.js
@@ -0,0 +1,35 @@
+'use strict'
+
+document.addEventListener('DOMContentLoaded', () => {
+  const feedbackForm = document.querySelector('.feedback-form');
+
+  feedbackForm.addEventListener('input', () => {
+    
+    const currentState = {
+      email: feedbackForm.elements.email.value,
+      message: feedbackForm.elements.message.value,
+    };
+    localStorage.setItem('feedback-form-state', JSON.stringify(currentState));
+  });
+
+  
+  const storedState = JSON.parse(localStorage.getItem('feedback-form-state')) || {};
+  feedbackForm.elements.email.value = storedState.email || '';
+  feedbackForm.elements.message.value = storedState.message || '';
+
+
+  feedbackForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+
+    const formValues = {
+      email: feedbackForm.elements.email.value,
+      message: feedbackForm.elements.message.value,
+    };
+    console.log(formValues);
+
+    
+    localStorage.removeItem('feedback-form-state');
+    feedbackForm.reset();
+  });
+});
